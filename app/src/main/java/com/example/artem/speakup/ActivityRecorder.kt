522b4a0c206714293fbfccc21bd0154ad6989305
ASUS <grazashar@mail.ru>
2018-03-18 03:18:17 +0200

    Base Text Analysis

--- app/src/main/java/com/example/artem/speakup/ActivityRecorder.kt
+++ app/src/main/java/com/example/artem/speakup/ActivityRecorder.kt
@@ -11,10 +11,15 @@ import com.arellomobile.mvp.presenter.InjectPresenter
 import com.github.mikephil.charting.data.*
 
 import kotlinx.android.synthetic.main.activity_recorder.*
+import ru.yandex.speechkit.Initializer
+import ru.yandex.speechkit.InitializerListener
+import ru.yandex.speechkit.SpeechKit
 import java.lang.Math.abs
 
 class ActivityRecorder : MvpAppCompatActivity(),
     RecorderPresenter.Interface {
+
+    private val API_KEY = "34e04a4d-07bc-4e70-8527-7b5e49f62cf9"
 
     @InjectPresenter
     lateinit var presenter: RecorderPresenter
@@ -26,9 +31,10 @@ class ActivityRecorder : MvpAppCompatActivity(),
         SpeechKit.getInstance().configure(applicationContext, API_KEY)
 
         Initializer.create(object : InitializerListener {
+            override fun onError(p0: Initializer?, p1: ru.yandex.speechkit.Error?) {}
             override fun onInitializerBegin(p0: Initializer?) {}
             override fun onInitializerDone(p0: Initializer?) {}
-            override fun onError(p0: Initializer?, p1: Error?) {}
+
         }).start()
 
         record_button.setOnClickListener({ _ -> recordHandler() })
--- app/src/main/java/com/example/artem/speakup/SpeechAnalysis/AnalysisPresenter.kt
+++ app/src/main/java/com/example/artem/speakup/SpeechAnalysis/AnalysisPresenter.kt
@@ -1,7 +1,7 @@
 package com.example.artem.speakup.SpeechAnalysis
 
 import android.annotation.SuppressLint
-import android.os.CountDownTimer
+import android.util.Log
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
 import com.arellomobile.mvp.MvpView
@@ -14,32 +14,19 @@ import ru.yandex.speechkit.RecognizerListener
 class AnalysisPresenter : MvpPresenter<AnalysisPresenter.AnalysisView>()   {
 
     private var recognizer: Recognizer? = null
-    private var cleanTextRes = StringBuffer("")
-    private var generalRes = StringBuffer("")
-    private var partialRes = StringBuffer("")
-    private var timer: CountDownTimer = object : CountDownTimer(3000, 1000) {
-        override fun onFinish() {
-            generalRes.append(" [Long pause] ")
-        }
+    private var cleanText = StringBuffer("")
 
-        override fun onTick(p0: Long) {
-        }
-
-    }
-
-    fun start() {
+    fun startRecognizer() {
         createAndStartRecognizer()
     }
 
-    fun stop() {
-        cancelRecognizer()
-        viewState.showResults(cleanTextRes.toString(), generalRes.toString())
-        cleanTextRes.delete(0, cleanTextRes.length)
-        generalRes.delete(0, generalRes.length)
-    }
-
-    private fun stopTimer() {
-        timer.cancel()
+    fun cancelRecognizer() {
+        if (recognizer != null) {
+            recognizer!!.cancel()
+            recognizer = null
+            Log.v("dfgdfg", "dfgdfg")
+            viewState.showResults(cleanText.toString())
+        }
     }
 
     @SuppressLint("MissingPermission")
@@ -50,30 +37,12 @@ class AnalysisPresenter : MvpPresenter<AnalysisPresenter.AnalysisView>()   {
 
             override fun onSoundDataRecorded(p0: Recognizer?, p1: ByteArray?) {}
 
-            override fun onPowerUpdated(p0: Recognizer?, p1: Float) {
-
-                when {
-                    p1 == 0f -> {
-                        timer.start()
-                    }
-                    p1 != 0f && partialRes.toString() == "" -> {
-                        timer.cancel()
-                        generalRes.append(" [Bad Speech] ")
-                    }else -> {
-                        timer.cancel()
-                    }
-                }
-            }
+            override fun onPowerUpdated(p0: Recognizer?, p1: Float) {}
 
             override fun onPartialResults(p0: Recognizer?, p1: Recognition?, p2: Boolean) {
-                if (p1 != null) {
-                    partialRes.append(p1.bestResultText)
+                if (p2) if (p1 != null) {
                     viewState.showPartialRes(p1.bestResultText)
-                    if (p2) {
-                        partialRes.delete(0, partialRes.length)
-                        cleanTextRes.append(p1.bestResultText + " ")
-                        generalRes.append(p1.bestResultText + " ")
-                    }
+                    cleanText.append(p1.bestResultText + " ")
                 }
             }
 
@@ -86,36 +55,27 @@ class AnalysisPresenter : MvpPresenter<AnalysisPresenter.AnalysisView>()   {
             override fun onRecognitionDone(p0: Recognizer?, p1: Recognition?) {}
 
             override fun onError(p0: Recognizer?, p1: Error?) {
-               if (p1 != null) when (p1.code) {
-                   Error.ERROR_CANCELED -> {
-                       viewState.showPartialRes("Cancelled")
-
-                   }
-                   Error.ERROR_NETWORK -> {
-                       viewState.showPartialRes("Network error")
-                   }
-                   Error.ERROR_SERVER -> {
-                       viewState.showPartialRes("Server recognizer error")
-                   }
-
-               }
+                if (p1 != null) when (p1.code) {
+                    Error.ERROR_CANCELED -> {
+                        viewState.showPartialRes("Cancelled")
+                    }
+                    Error.ERROR_NETWORK -> {
+                        viewState.showPartialRes("Network error")
+                    }
+                    Error.ERROR_SERVER -> {
+                        viewState.showPartialRes("Server recognizer error")
+                    }
+                }
                 cancelRecognizer()
             }
 
-        }, true )
+        }, true)
         recognizer!!.start()
-    }
-
-    private fun cancelRecognizer() {
-        if (recognizer != null) {
-            recognizer!!.cancel()
-            recognizer = null
-        }
     }
 
 
     interface AnalysisView : MvpView {
         fun showPartialRes(text: String)
-        fun showResults(cleanText: String, generalText: String)
+        fun showResults(cleanText: String)
     }
 }
